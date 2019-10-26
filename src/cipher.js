96a950cf88a711248e364a9071a5df3076b489e9
AryMF <ariadne.hp@gmail.com>
2019-10-25 23:56:59 -0500

    Funcion encode y decode con DOM data

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,10 @@
 window.cipher = {
-  // Acá escribe tu código.
+  // Función para codificar
   encode: (offset, string) => {
-    
     let result = [];
-    // let charCodeArray= [];
     let offsetCalculated = offset % 26;
     offsetCalculated = offsetCalculated < 0 ? 26 + offsetCalculated : offsetCalculated;
-    alert(offsetCalculated);
 
-    
     for(let i=0;i<string.length;i++){
       switch(true){
         case string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90:
@@ -25,10 +21,33 @@ window.cipher = {
         break;
       }
     }
-    alert(string + " es " + result);
+    //alert("Encode: " + string + " es " + result);
+    document.getElementById("cipherResult").value = result.join("");
   },
-
+  //Función para decodificar
   decode: (offset, string) => {
-    //kek
+    let result = [];
+    let offsetCalculated = offset % 26;
+    offsetCalculated = offsetCalculated < 0 ? -1 * offsetCalculated : 26 - offsetCalculated;
+    //alert(offsetCalculated);
+
+    for(let i=0;i<string.length;i++){
+      switch(true){
+        case string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90:
+          //cipher mayúsculas
+          result.push(String.fromCharCode((string.charCodeAt(i) - 65 + offsetCalculated) % 26 + 65));
+        break;
+        case string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122:
+          //cipher minúsculas
+          result.push(String.fromCharCode((string.charCodeAt(i) - 97 + offsetCalculated) % 26 + 97));
+        break;
+        default:
+          //ignorar todo lo demás
+          result.push(string[i]);
+        break;
+      }
+    }
+    //alert("Decode: " + string + " es " + result);
+    document.getElementById("cipherFill").value = result.join("");
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
- const doIt = () => {
+ const doIt = (option) => {
+    option == 1 ? window.cipher.encode(-2, document.getElementById("cipherFill").value) 
+        : window.cipher.decode(-2,document.getElementById("cipherResult").value);
 
-    window.cipher.encode(-15,"abcdefghijklmnopqrstuvwxyz");
+    
 }
