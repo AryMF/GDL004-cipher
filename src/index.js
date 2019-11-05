a75cf525bec12f69ebeb888cec20bc78369732bc
AryMF <ariadne.hp@gmail.com>
2019-11-04 22:26:39 -0600

    Cambios para legibilidad del código

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Declaración de variables y constantes
 let lastTextAreaChanged=0;
 const delayTime = 2000;
 const offsetOption = 0;
@@ -7,26 +8,34 @@ const stepDownOption = 1;
 const stepUpOption = 2;
 
 const getDivTransition = document.getElementById("divTransition");
-const getOffsetNumber = document.getElementById("offsetNumber");
+const offsetNumberInput = document.getElementById("offsetNumber");
 const getCipherFill = document.getElementById("cipherFill");
 const getDecipherFill = document.getElementById("decipherFill");
 const getCipherDecodeTitle = document.getElementById("cipherDecodeTitle");
 const getCipherEncodeTitle = document.getElementById("cipherEncodeTitle");
 
+const doOffsetOption = () => { doIt(offsetOption); };
+const doCipherOption = () => { doIt(cipherOption); };
+const doDecipherOption = () => { doIt(decipherOption); };
+const spinBootstrapStepUP = () => { spinBootstrap(stepUpOption); };
+const spinBootstrapStepDown = () => { spinBootstrap(stepDownOption); };
+
+//Funciónes para inicializar los EventListener
 const main = () => {
     document.getElementById("StartButton").addEventListener("click", showSecondScreen);
-    document.getElementById("spinBootstrapSubtract").addEventListener("click", function(){ spinBootstrap(stepDownOption); });
-    document.getElementById("spinBootstrapAdd").addEventListener("click", function(){ spinBootstrap(stepUpOption); });
+    document.getElementById("spinBootstrapSubtract").addEventListener("click", spinBootstrapStepDown);
+    document.getElementById("spinBootstrapAdd").addEventListener("click", spinBootstrapStepUP);
     document.getElementById("swapButton").addEventListener("click", swap);
-    getOffsetNumber.addEventListener("input", function(){ doIt(offsetOption); });
-    getCipherFill.addEventListener("input", function(){ doIt(cipherOption); });
-    getDecipherFill.addEventListener("input", function(){ doIt(decipherOption); });
+    offsetNumberInput.addEventListener("input", doOffsetOption);
+    getCipherFill.addEventListener("input", doCipherOption);
+    getDecipherFill.addEventListener("input", doDecipherOption);
 };
 
 window.onload = function(){
     main();
 };
 
+//Función realiza el efecto de transición y manda llamar a la segunda pantalla
 const showSecondScreen = () => {
     getDivTransition.style.visibility = "visible";
     getDivTransition.classList.remove("transition");
@@ -37,25 +46,28 @@ const showSecondScreen = () => {
     }, delayTime);
 };
 
+//Función que manda llamar a cipher e imprime el resultado
 const doIt = (option) => {
-    option == 0 ? option = lastTextAreaChanged : option;
+     option == 0 ? option = lastTextAreaChanged : option;
 
     if(option==1){
-        document.getElementById("decipherFill").value = window.cipher.encode(getOffsetNumber.value, getCipherFill.value);
+        document.getElementById("decipherFill").value = window.cipher.encode(offsetNumberInput.value, getCipherFill.value);
     }
     else {
-        document.getElementById("cipherFill").value = window.cipher.decode(getOffsetNumber.value, getDecipherFill.value);
+        document.getElementById("cipherFill").value = window.cipher.decode(offsetNumberInput.value, getDecipherFill.value);
     }
 
     lastTextAreaChanged = option;
 };
 
+//Función para aumenta o decrementar un numero en el input del offset
 const spinBootstrap = (option) => {
-    option == 1 ? getOffsetNumber.stepDown(stepDownOption) 
-        : getOffsetNumber.stepUp(stepUpOption);
+    option === 1 ? offsetNumberInput.stepDown(stepDownOption) 
+        : offsetNumberInput.stepUp(stepUpOption);
     doIt(offsetOption);
 };
 
+//Función para cambiar entre modalidades cipher/decipher
 const swap = () => {
     if(getCipherDecodeTitle.style.visibility == "hidden" ){
         getCipherDecodeTitle.style.visibility = "visible";
